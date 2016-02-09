a381f04df8cb295e49b1a71137d76de55078c573
Jim <dntalaperas@ubitech.eu>
2016-02-09 15:28:29 +0200

    XML Response is now marshalled according to HAVE model

--- src/main/java/org/impress/datahomogenization/ws/web/api/BedsAvailabilityQueryController.java
+++ src/main/java/org/impress/datahomogenization/ws/web/api/BedsAvailabilityQueryController.java
@@ -1,5 +1,14 @@
 package org.impress.datahomogenization.ws.web.api;
 
+import java.io.StringWriter;
+import java.math.BigInteger;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+
 import org.impress.datahomogenization.ws.model.BedStats;
 import org.impress.datahomogenization.ws.service.BedsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import oasis.names.tc.emergency.edxl.have._1.Capacity;
+
 @RestController
 public class BedsAvailabilityQueryController {
 	@Autowired
@@ -19,9 +30,34 @@ public class BedsAvailabilityQueryController {
 	@RequestMapping(
 			value="/beds/available/{clinicId}", 
 			method=RequestMethod.GET,
-			produces={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-	public ResponseEntity<BedStats> getAvailableBeds(@PathVariable("clinicId") Integer clinicId) {	
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<BedStats> getAvailableBedsJson(@PathVariable("clinicId") Integer clinicId) {	
 		BedStats bedStats = bedService.getClinicAvailableBeds(clinicId);
 		return new ResponseEntity<BedStats>(bedStats, HttpStatus.OK);
 	}
+	@RequestMapping(
+			value="/beds/available/{clinicId}", 
+			method=RequestMethod.GET,
+			produces=MediaType.APPLICATION_XML_VALUE)
+	public ResponseEntity<String> getAvailableBedsXML(@PathVariable("clinicId") Integer clinicId) {	
+		BedStats bedStats = bedService.getClinicAvailableBeds(clinicId);
+		Capacity capacity = new Capacity();
+		capacity.setAvailableCount(BigInteger.valueOf(bedStats.getAvailabeBeds()));
+		String response = "";
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(Capacity.class);
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			StringWriter sw = new StringWriter();
+			//jaxbMarshaller.marshal(capacity, sw);
+			jaxbMarshaller.marshal(
+					new JAXBElement<Capacity>(new QName("","rootTag"),Capacity.class, capacity), sw);
+			response = sw.toString();
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>("Error Marshalling XML Object", HttpStatus.INTERNAL_SERVER_ERROR);
+		}		
+		//return new ResponseEntity<BedStats>(bedStats, HttpStatus.OK);
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+	}	
 }
